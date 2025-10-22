83c17f3afc32644dd906a1d86f241e4a8eb3d8cd
chunkind <chunkind@naver.com>
2025-10-22 18:56:50 +0900

    10/22: 초기 셋팅

---
+++ GameCoding/GameCoding.cpp
@@ -0,0 +1,131 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "GameCoding.h"
+#include "Game.h"
+
+HINSTANCE hInst;
+HWND g_hWnd;
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    // 1) 윈도우 창 정보 등록
+    MyRegisterClass(hInstance);
+
+    // 2) 윈도우 창 생성
+    if (!InitInstance(hInstance, nCmdShow))
+        return FALSE;
+
+    Game game;
+    game.Init(g_hWnd);
+
+    MSG msg = {};
+    uint64 prevTick = 0;
+
+    // 3) 메인 루프
+    // - 입력
+    // - 로직
+    // - 렌더링
+    while (WM_QUIT != msg.message)
+    {
+        if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+        }
+        else
+        {
+            uint64 now = ::GetTickCount64();
+
+            if (now - prevTick >= 10) {
+                game.Update();
+                game.Render();
+
+                prevTick = now;
+            }
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = ::LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GAMECODING));
+    wcex.hCursor = ::LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"key";
+    wcex.hIconSm = ::LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return ::RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance;
+
+    RECT windowRect = { 0, 0, 800, 600 };
+    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, false);
+
+    g_hWnd = ::CreateWindowW(L"key", L"Client", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, nullptr, nullptr, hInstance, nullptr);
+
+    if (!g_hWnd)
+    {
+        return FALSE;
+    }
+
+    ::ShowWindow(g_hWnd, nCmdShow);
+    ::UpdateWindow(g_hWnd);
+
+    return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
+        {
+        case IDM_EXIT:
+            ::DestroyWindow(hWnd);
+            break;
+        default:
+            return ::DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = ::BeginPaint(hWnd, &ps);
+        ::EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        break;
+    default:
+        return ::DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
---
+++ GameCoding/InputManager.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include "InputManager.h"
+
+void InputManager::Init(HWND hwnd)
+{
+	_hwnd = hwnd;
+	_states.resize(KEY_TYPE_COUNT, KeyState::None);
+}
+
+void InputManager::Update()
+{
+	BYTE asciiKeys[KEY_TYPE_COUNT] = {};
+	if (::GetKeyboardState(asciiKeys) == false)
+		return;
+
+	for (uint32 key = 0; key < KEY_TYPE_COUNT; key++)
+	{
+		// 키가 눌려 있으면 true
+		if (asciiKeys[key] && 0x80) // 16진수 0x80 => 10진수 128
+		{
+			KeyState& state = _states[key];
+
+			// 이전 프레임에 키를 누른상태라면 PRESS
+			if (state == KeyState::Press || state == KeyState::Down)
+				state = KeyState::Press;
+			else
+				state = KeyState::Down;
+		}
+		else
+		{
+			KeyState& state = _states[key];
+
+			// 이전 프레임에 키를 누른상태라면 Up
+			if (state == KeyState::Press || state == KeyState::Down)
+				state = KeyState::Up;
+			else
+				state = KeyState::None;
+		}
+	}
+
+	// Mouse
+	::GetCursorPos(&_mousePos); // 커서 좌표를 알아온다.
+	::ScreenToClient(_hwnd, &_mousePos);
+}
