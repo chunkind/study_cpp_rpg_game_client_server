22dc3bd6adff01aca2bb6d46e3c0277b62983adf
chunkind <chunkind@naver.com>
2025-10-25 17:56:22 +0900

    10/25: Scene, SceneManager

--- GameCoding/Defines.h
+++ GameCoding/Defines.h
@@ -11,3 +11,10 @@ public:									\
 	}
 
 #define GET_SINGLE(classname) classname::GetInstance()
+
+#define SAFE_DELETE(ptr)				\
+	if (ptr)							\
+	{									\
+		delete ptr;						\
+		ptr = nullptr;					\
+	}
--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -2,6 +2,7 @@
 #include "Game.h"
 #include "TimeManager.h"
 #include "InputManager.h"
+#include "SceneManager.h"
 
 Game::Game()
 {
@@ -9,7 +10,9 @@ Game::Game()
 }
 Game::~Game()
 {
+	GET_SINGLE(SceneManager)->Clear();
 
+	_CrtDumpMemoryLeaks();
 }
 
 void Game::Init(HWND hwnd)
@@ -19,11 +22,15 @@ void Game::Init(HWND hwnd)
 
 	GET_SINGLE(TimeManager)->Init();
 	GET_SINGLE(InputManager)->Init(hwnd);
+	GET_SINGLE(SceneManager)->Init();
+
+	GET_SINGLE(SceneManager)->ChangeScene(SceneType::DevScene);
 }
 void Game::Update()
 {
 	GET_SINGLE(TimeManager)->Update();
 	GET_SINGLE(InputManager)->Update();
+	GET_SINGLE(SceneManager)->Update();
 }
 void Game::Render()
 {
@@ -40,5 +47,10 @@ void Game::Render()
 		wstring str = std::format(L"FPS({0}), DT({1}) ms)", fps, static_cast<int32> (deltaTime * 1000));
 		::TextOut(_hdc, 650, 10, str.c_str(), static_cast<int32>(str.size()));
 	}
-	::Rectangle(_hdc, 200, 200, 400, 400);
+
+	GET_SINGLE(SceneManager)->Render(_hdc);
+
+	//::InvalidateRect(_hwnd, nullptr, true);
+
+	Utils::DrawRect(_hdc, Pos{ 400, 300 }, 1000, 1000);
 }
